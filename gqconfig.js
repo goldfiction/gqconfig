76f1e906703cee1cc0b8e957d7b796087f879400
Your Name <glidev5@gmail.com>
2017-03-06 21:48:08 +0000

    update

--- gqconfig.js
+++ gqconfig.js
@@ -2,16 +2,52 @@
  * Created by happy on 3/4/17.
  */
 var _ = require('lodash');
-require('require-ini');
+var ini = require('ini');
+var fs = require('fs');
 
+//----------------- main
 function config(o) {
     o = o || {};
+
     o.configFile = o.configFile || (process.cwd() + "/config.ini");
     o.defaultConfigFile = o.defaultConfigFile || (process.cwd() + "/default.config.ini");
-    o.customConfig = require(o.configFile);
-    o.defaultConfig = require(o.defaultConfigFile);
-    o.config = global.config = _.merge(_.cloneDeep(o.defaultConfig), _.cloneDeep(o.customConfig));
+
+    try {
+        o.customConfig = ini.parse(fs.readFileSync(o.configFile, 'utf-8'));
+    } catch (e) {
+        o.customConfig = {};
+    }
+    try {
+        o.defaultConfig = ini.parse(fs.readFileSync(o.defaultConfigFile, 'utf-8'));
+    } catch (e) {
+        o.defaultConfig = {};
+    }
+
+    o.config = _.merge(_.cloneDeep(o.defaultConfig), _.cloneDeep(o.customConfig));
+    o.config = doParse(o.config);
+    global.config = o.config;
+
     return o;
 }
 
 module.exports = config;
+
+//----------------------- libz
+
+function doParse(obj) {
+    try {
+        if (typeof obj === 'string' || obj instanceof String) {
+            obj = JSON.parse(obj);
+        }
+    } catch (e) {
+    }
+
+    if (typeof obj === 'object') {
+        for (var i in obj) {
+            if (typeof obj[i] === 'string' || obj[i] instanceof String || obj[i] instanceof Object) {
+                obj[i] = doParse(obj[i]);
+            }
+        }
+    }
+    return obj;
+}
--- test/test.js
+++ test/test.js
@@ -9,10 +9,10 @@ it("should be able to load config",function(done){
     setTimeout(function(){
         cfg=loadConfig({
             configFile:process.cwd()+"/test/config.ini",
-            defaultConfigFile:process.cwd()+"/test/default.config.ini",
+            defaultConfigFile:process.cwd()+"/test/default.config.ini"
         });
         console.log(cfg);
-        assert(global.config.general.a==='1');
+        assert(global.config.general.c===5);
         done();
     },10)
 });
